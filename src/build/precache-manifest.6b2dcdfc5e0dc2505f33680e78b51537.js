f801c3551bee87b27851e6dcc4bc127045892292
Michael Gathogo <mcmikegath@yahoo.com>
2020-09-23 11:21:52 -0400

    first commit

---
+++ src/build/precache-manifest.6b2dcdfc5e0dc2505f33680e78b51537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb65c09112441de178f17b7913a49f8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b25ac0abcd6c0fbacc",
+    "url": "/static/css/main.ab59d78a.chunk.css"
+  },
+  {
+    "revision": "add4332fab9f6c0f2f6c",
+    "url": "/static/js/2.51f5be15.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.51f5be15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b25ac0abcd6c0fbacc",
+    "url": "/static/js/main.99cec7c5.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
